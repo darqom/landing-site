b3f0048c607c8c501138e5f13cf235951e98784e
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2020-09-28 15:37:57 +0700

    Menambah owl carousel agar lebih responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,17 @@ $(document).ready(function(){
 		}
 	});
 });
+
+$('.owl-carousel').owlCarousel({
+	loop: true,
+	autoplay: true,
+	dots: true,
+	responsive: {
+		0: {
+			items: 1
+		},
+		560: {
+			items: 4
+		}
+	}
+})
